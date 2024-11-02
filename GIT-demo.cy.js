24e5dee8dd18ca633f0fae891dca786102576c2e
Yasin-QA <yasinm.Tester@gmail.com>
2024-11-02 17:45:32 +0530

    Update GIT-demo.cy.js

--- GIT-demo.cy.js
+++ GIT-demo.cy.js
@@ -4,6 +4,5 @@ cy.visit('www.demo page')
 cy.get('#input-name').type('Username')
 cy.get('input-password').type('password')
 cy.get('submit-button').click().should('be'.'visible')
-
-})
-})
+});
+});
